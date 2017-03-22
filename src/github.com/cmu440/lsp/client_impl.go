7aad7b4e4328d67fedc26920d67fa0d0d2573434
Iris <Iris>
2017-03-22 14:57:18 -0400

    my implementation

--- src/github.com/cmu440/lsp/client_impl.go
+++ src/github.com/cmu440/lsp/client_impl.go
@@ -2,10 +2,46 @@
 
 package lsp
 
-import "errors"
+import (
+	"github.com/cmu440/lspnet"
+	"time"
+)
 
 type client struct {
-	// TODO: implement this!
+	conn     *lspnet.UDPConn
+	connFin  chan *struct{}
+	closeSig bool
+	connID   int
+
+	// epoch
+	epochLimit  int
+	epochMillis int
+	windowSize  int
+	epochCnt    int
+	lastEpoch   int
+	epochChan   chan struct{}
+	// msg from clients
+	dataMsgChan chan *Message
+	ackMsgChan  chan *Message
+	// for Write()
+	wReqChan chan writeReq
+	wRepChan chan error
+	// for Read()
+	readBuffer []*readRep
+	rReqFlag   bool
+	rReqChan   chan struct{}
+	rRepChan   chan *readRep
+	//for Close()
+	closeConnReqChan chan struct{}
+	closeConnRepChan chan error
+	exitChan         chan struct{}
+
+	seqSend       int              // for seq of outgoing data msg
+	seqRecv       int              // seq of data msg received
+	seqAck        int              // msg acked so far
+	writeBuffer   []*Message       // store msg not in window
+	notAckMsgList map[int]*Message // store data sent but not acked
+	recvDataList  map[int]*Message // store data msg received but not in order
 }
 
 // NewClient creates, initiates, and returns a new client. This function
@@ -15,27 +51,332 @@ type client struct {
 // connection could not be made (i.e., if after K epochs, the client still
 // hasn't received an Ack message from the server in response to its K
 // connection requests).
-//
+
 // hostport is a colon-separated string identifying the server's host address
 // and port number (i.e., "localhost:9999").
 func NewClient(hostport string, params *Params) (Client, error) {
-	return nil, errors.New("not yet implemented")
+	//Connect udp
+	addr, err := lspnet.ResolveUDPAddr("udp", hostport)
+	if err != nil {
+		return nil, err
+	}
+	conn, err := lspnet.DialUDP("udp", nil, addr)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &client{
+		conn:             conn,
+		connFin:          make(chan *struct{}),
+		closeSig:         false,
+		connID:           -1,
+		epochCnt:         0,
+		lastEpoch:        0,
+		epochChan:        make(chan struct{}),
+		dataMsgChan:      make(chan *Message),
+		ackMsgChan:       make(chan *Message),
+		wReqChan:         make(chan writeReq),
+		wRepChan:         make(chan error),
+		readBuffer:       make([]*readRep, 0),
+		rReqFlag:         false,
+		rReqChan:         make(chan struct{}),
+		rRepChan:         make(chan *readRep),
+		closeConnReqChan: make(chan struct{}),
+		closeConnRepChan: make(chan error),
+		exitChan:         make(chan struct{}),
+		seqSend:          0,
+		seqRecv:          0,
+		seqAck:           0,
+		writeBuffer:      make([]*Message, 0),
+		notAckMsgList:    make(map[int]*Message),
+		recvDataList:     make(map[int]*Message),
+	}
+	if params == nil {
+		params = NewParams()
+	}
+	c.epochLimit = params.EpochLimit
+	c.epochMillis = params.EpochMillis
+	c.windowSize = params.WindowSize
+
+	msg := NewConnect()
+	sendMsgClient(msg, conn)
+	c.notAckMsgList[0] = msg
+
+	go c.mainRoutine()
+	go c.receiveMsgRoutine()
+	go c.epochRoutine()
+
+	rep := <-c.connFin
+	if rep == nil {
+		conn.Close()
+		return nil, connFailErr
+	}
+	return c, nil
+}
+
+func (c *client) handleAckMsg(msg *Message) {
+	c.lastEpoch = c.epochCnt
+	if _, ok := c.notAckMsgList[msg.SeqNum]; ok {
+		delete(c.notAckMsgList, msg.SeqNum)
+
+		if msg.SeqNum == 0 {
+			c.connID = msg.ConnID
+			c.connFin <- &struct{}{}
+		} else if c.connID > 0 {
+			// trigger sliding window shift
+			c.seqAck++
+			if len(c.writeBuffer) > 0 {
+				m := c.writeBuffer[0]
+				sendMsgClient(m, c.conn)
+				// put this new sent msg into not ack list
+				c.notAckMsgList[m.SeqNum] = m
+				c.writeBuffer = c.writeBuffer[1:]
+
+			}
+		}
+	}
+	if c.closeSig &&
+		len(c.notAckMsgList) == 0 && len(c.writeBuffer) == 0 {
+		if c.rReqFlag {
+			c.rRepChan <- &readRep{0, nil, clientClosedErr}
+			c.rReqFlag = false
+		}
+		c.closeConnRepChan <- nil
+	}
+}
+
+func (c *client) handleDataMsg(msg *Message) {
+	c.lastEpoch = c.epochCnt
+	if c.connID < 0 || c.closeSig {
+		return
+	}
+	if len(msg.Payload) < msg.Size { // should be dropped as never received
+		return
+	} else if len(msg.Payload) > msg.Size {
+		msg.Payload = msg.Payload[:msg.Size]
+	}
+
+	ackmsg := NewAck(c.connID, msg.SeqNum)
+	sendMsgClient(ackmsg, c.conn)
+
+	if msg.SeqNum == c.seqRecv+1 { // expected sequence
+		c.seqRecv++
+		c.readBuffer = append(c.readBuffer, &readRep{0, msg.Payload, nil})
+		l := len(c.recvDataList)
+		for i := 0; i < l; i++ {
+			if m, ok := c.recvDataList[c.seqRecv+1]; ok {
+				c.seqRecv++
+				c.readBuffer = append(c.readBuffer, &readRep{0, m.Payload, nil})
+				delete(c.recvDataList, c.seqRecv)
+			} else {
+				break
+			}
+		}
+	} else if msg.SeqNum > c.seqRecv { // order mixed up
+		c.recvDataList[msg.SeqNum] = msg
+	} // else meaning already received
+
+	if c.rReqFlag {
+		c.handleReadReq()
+	}
+}
+
+func (c *client) handleReadReq() {
+	if c.closeSig {
+		c.rReqFlag = false
+		c.rRepChan <- &readRep{0, nil, clientClosedErr}
+		return
+	}
+	if len(c.readBuffer) > 0 {
+		c.rReqFlag = false
+		c.rRepChan <- &readRep{0, c.readBuffer[0].payload, nil}
+
+		// remove the msg
+		c.readBuffer = c.readBuffer[1:]
+
+	}
+}
+
+func (c *client) handleWriteReq(wq writeReq) {
+	payload := wq.payload
+	var err error = nil
+	if c.closeSig {
+		err = clientClosedErr
+	}
+	c.wRepChan <- err
+	if err != nil {
+		return
+	}
+	c.seqSend++
+	msg := NewData(c.connID, c.seqSend, len(payload), payload)
+	if msg.SeqNum <= c.seqAck+c.windowSize {
+		c.notAckMsgList[msg.SeqNum] = msg
+		sendMsgClient(msg, c.conn)
+	} else {
+		c.writeBuffer = append(c.writeBuffer, msg)
+	}
+}
+
+func (c *client) mainRoutine() {
+	for {
+		select {
+		case <-c.exitChan:
+			return
+		case msg := <-c.dataMsgChan: // chan closed at Close()
+			c.handleDataMsg(msg)
+		case msg := <-c.ackMsgChan:
+			c.handleAckMsg(msg)
+		case <-c.rReqChan:
+			c.rReqFlag = true
+			c.handleReadReq()
+		case wq := <-c.wReqChan:
+			c.handleWriteReq(wq)
+		case <-c.epochChan:
+			c.handleEpoch()
+		case <-c.closeConnReqChan:
+			c.closeSig = true
+			if len(c.writeBuffer) == 0 && len(c.notAckMsgList) == 0 {
+				if c.rReqFlag {
+					c.rReqFlag = false
+					c.rRepChan <- &readRep{0, nil, clientClosedErr}
+				}
+				c.closeConnRepChan <- nil
+			}
+		}
+	}
+}
+
+// readFromUDP will block until some msg comes
+// start a go routine to handle incoming msg
+func (c *client) receiveMsgRoutine() {
+	for {
+		select {
+		case <-c.exitChan:
+			return
+		default:
+			msg := recvMsgClient(c.conn)
+			if msg == nil { // ignore wrong msg
+				continue
+			}
+			switch msg.Type {
+			case MsgData:
+				c.dataMsgChan <- msg
+			case MsgAck:
+				c.ackMsgChan <- msg
+			default: //ignore undefined msg type
+				logger.Println(undefMsgTypeErr)
+			}
+		}
+	}
+}
+
+func (c *client) epochRoutine() {
+	for {
+		select {
+		case <-c.exitChan:
+			return
+		case <-time.After(time.Duration(c.epochMillis) * time.Millisecond):
+			c.epochChan <- struct{}{}
+		}
+	}
+}
+
+// handle epoch, invoked by mainRoutine:
+// 1) If the client’s connection request has not yet been acknowledged by the server,
+// resend the connection request.
+// 2) If the connection request has been sent and acknowledged, but no data messages
+// received, then send an acknowledgment with sequence number 0.
+// 3) For each data message that has been sent but not yet acknowledged, resend
+// the data message.
+func (c *client) handleEpoch() {
+	c.epochCnt++
+
+	// if not receiving any msg more than epochLimit, then conn lost
+	if c.epochCnt-c.lastEpoch > c.epochLimit {
+		if c.rReqFlag {
+			c.rReqFlag = false
+			c.rRepChan <- &readRep{0, nil, clientLostErr}
+		}
+		if c.closeSig {
+			c.closeConnRepChan <- clientLostErr
+		}
+		return
+	}
+	// 2) if no data msg have been received
+	if c.connID > 0 && c.seqRecv == 0 {
+		msg := NewAck(c.connID, 0)
+		sendMsgClient(msg, c.conn)
+	}
+	// 1), 3) for every msg that sent but not acked, resend
+	// connection msg is also included in the list
+	for _, v := range c.notAckMsgList {
+		sendMsgClient(v, c.conn)
+	}
+
 }
 
 func (c *client) ConnID() int {
-	return -1
+	return c.connID
 }
 
+// Read reads a data message from the server and returns its payload.
+// This method should block until data has been received from the server and
+// is ready to be returned. It should return a non-nil error if either
+// (1) the connection has been explicitly closed, or
+// (2) the connection has been lost due to an epoch timeout and no other
+// messages are waiting to be returned.
 func (c *client) Read() ([]byte, error) {
-	// TODO: remove this line when you are ready to begin implementing this method.
-	select {} // Blocks indefinitely.
-	return nil, errors.New("not yet implemented")
+	c.rReqChan <- struct{}{}
+	reply := <-c.rRepChan
+	return reply.payload, reply.err
 }
 
+// Write sends a data message with the specified payload to the server.
+// This method should NOT block, and should return a non-nil error
+// if the connection with the server has been lost.
 func (c *client) Write(payload []byte) error {
-	return errors.New("not yet implemented")
+	if len(payload) > MAX_MSG_SIZE {
+		return msgTooLargeErr
+	}
+	c.wReqChan <- writeReq{0, payload}
+	err := <-c.wRepChan
+	return err
 }
 
+// Close terminates the client's connection with the server. It should block
+// until all pending messages to the server have been sent and acknowledged.
+// Once it returns, all goroutines running in the background should exit.
+//
+// You may assume that Read, Write, and Close will not be called after
+// Close has been called.
 func (c *client) Close() error {
-	return errors.New("not yet implemented")
+	c.closeConnReqChan <- struct{}{}
+	err := <-c.closeConnRepChan
+	close(c.exitChan)
+	c.conn.Close()
+	return err
+}
+
+func sendMsgClient(msg *Message, conn *lspnet.UDPConn) {
+	b := MarshalMsg(msg)
+	if b == nil {
+		// suggest sth wrong with the implementation
+		logger.Fatalln("Fatal error in Marshal at sendMsg")
+	}
+	// WriteToUDP is only good for server, on client should use Write
+	// No error will be thrown and no blocking even if connection has lost
+	conn.Write(b)
+}
+
+func recvMsgClient(conn *lspnet.UDPConn) *Message {
+	buffer := make([]byte, 1024)
+	size, err := conn.Read(buffer)
+	if err != nil { // error in Read, might caused by connection closed
+		return nil
+	}
+	msg := UnmarshalMsg(buffer[:size]) // if any error in unmarshal, msg = nil
+	if msg == nil {
+		logger.Println("Error in Unmarshal at recvMsg", size)
+	}
+	return msg
 }
--- src/github.com/cmu440/lsp/server_impl.go
+++ src/github.com/cmu440/lsp/server_impl.go
@@ -2,10 +2,67 @@
 
 package lsp
 
-import "errors"
+import (
+	"github.com/cmu440/lspnet"
+	"strconv"
+	"time"
+)
 
 type server struct {
-	// TODO: implement this!
+	conn          *lspnet.UDPConn
+	curID         int // for connID
+	clientCnt     int
+	closeSig      bool
+	clientAddrMap map[*lspnet.UDPAddr]bool // track addr and client
+	clientIdMap   map[int]*clientInfo      // connID as key
+	// epoch
+	epochLimit  int
+	epochMillis int
+	windowSize  int
+	epochCnt    int
+	epochChan   chan struct{}
+	// msg from clients
+	connMsgChan chan *msgWrapper // connection msg has no content
+	dataMsgChan chan *msgWrapper
+	ackMsgChan  chan *msgWrapper
+	// for Write()
+	wReqChan chan writeReq
+	wRepChan chan error
+	// for Read()
+	readBuffer []*readRep
+	rReqFlag   bool
+	rReqChan   chan struct{}
+	rRepChan   chan *readRep
+	//for closeConn()
+	closeConnReqChan chan int
+	closeConnRepChan chan error
+	// for Close()
+	shutdownAll    chan struct{}
+	shutdownAllErr chan error
+	shutdownAllFin chan struct{}
+	exitChan       chan struct{}
+}
+
+type clientInfo struct {
+	connID        int
+	addr          *lspnet.UDPAddr
+	closeSig      bool
+	isLost        bool
+	isClosed      bool
+	seqSend       int              // number of data msg sent to client
+	seqAck        int              // number of ack msg from client
+	writeBuffer   []*Message       // store msg not in window
+	notAckMsgList map[int]*Message // store data sent but not acked
+	seqRecv       int              // number of data msg from client
+	recvDataList  map[int]*Message // store data msg received but not in order
+	lastEpoch     int              // compare with s.epochCnt to judge lost
+}
+
+// used to check clientList[msg.connID].addr == addr
+// security issue: if an addr use another client's connID
+type msgWrapper struct {
+	msg  *Message
+	addr *lspnet.UDPAddr
 }
 
 // NewServer creates, initiates, and returns a new server. This function should
@@ -15,23 +72,426 @@ type server struct {
 // project 0, etc.) and immediately return. It should return a non-nil error if
 // there was an error resolving or listening on the specified port number.
 func NewServer(port int, params *Params) (Server, error) {
-	return nil, errors.New("not yet implemented")
+	// listen to incoming udp packets
+	addr, err := lspnet.ResolveUDPAddr("udp", ":"+strconv.Itoa(port))
+	if err != nil {
+		return nil, err
+	}
+	conn, err := lspnet.ListenUDP("udp", addr)
+	if err != nil {
+		return nil, err
+	}
+	s := server{
+		conn:             conn,
+		curID:            0,
+		clientCnt:        0,
+		closeSig:         false,
+		clientAddrMap:    make(map[*lspnet.UDPAddr]bool),
+		clientIdMap:      make(map[int]*clientInfo),
+		epochCnt:         0,
+		epochChan:        make(chan struct{}),
+		connMsgChan:      make(chan *msgWrapper),
+		dataMsgChan:      make(chan *msgWrapper),
+		ackMsgChan:       make(chan *msgWrapper),
+		wReqChan:         make(chan writeReq),
+		wRepChan:         make(chan error),
+		readBuffer:       make([]*readRep, 0),
+		rReqFlag:         false,
+		rReqChan:         make(chan struct{}),
+		rRepChan:         make(chan *readRep),
+		closeConnReqChan: make(chan int),
+		closeConnRepChan: make(chan error),
+		shutdownAll:      make(chan struct{}),
+		shutdownAllErr:   make(chan error),
+		shutdownAllFin:   make(chan struct{}),
+		exitChan:         make(chan struct{}),
+	}
+
+	if params == nil {
+		params = NewParams()
+	}
+	s.epochLimit = params.EpochLimit
+	s.epochMillis = params.EpochMillis
+	s.windowSize = params.WindowSize
+
+	// go routines
+	go s.mainRoutine()
+	go s.receiveMsgRoutine()
+	go s.epochRoutine()
+	return &s, nil
 }
 
+// invoked by mainRoutine, handle connection msg
+func (s *server) handleConnMsg(wrapper *msgWrapper) {
+	if s.closeSig {
+		return
+	}
+	addr := wrapper.addr
+	if s.clientAddrMap[addr] { // if already exist
+		return
+	}
+	s.clientAddrMap[addr] = true
+	s.curID++
+	s.clientCnt++
+
+	cli := &clientInfo{
+		connID:        s.curID,
+		addr:          addr,
+		closeSig:      false,
+		isLost:        false,
+		isClosed:      false,
+		seqSend:       0,
+		seqAck:        0,
+		seqRecv:       0,
+		notAckMsgList: make(map[int]*Message),
+		recvDataList:  make(map[int]*Message),
+		writeBuffer:   make([]*Message, 0),
+		lastEpoch:     s.epochCnt,
+	}
+	s.clientIdMap[s.curID] = cli
+	msg := NewAck(cli.connID, 0)
+	sendMsg(msg, addr, s.conn)
+}
+
+//invokde by mainRoutine, handle data msg
+func (s *server) handleDataMsg(wrapper *msgWrapper) {
+	addr := wrapper.addr
+	msg := wrapper.msg
+	cli := s.clientIdMap[msg.ConnID]
+	cli.lastEpoch = s.epochCnt
+
+	// check addr the same to avoid potential security issue
+	if s.closeSig || cli.closeSig || cli.isLost || cli.isClosed ||
+		cli.addr.String() != addr.String() {
+		return
+	}
+
+	if len(msg.Payload) < msg.Size { // should be dropped as never received
+		return
+	} else if len(msg.Payload) > msg.Size {
+		msg.Payload = msg.Payload[:msg.Size]
+	}
+
+	ackmsg := NewAck(msg.ConnID, msg.SeqNum)
+	sendMsg(ackmsg, cli.addr, s.conn)
+
+	if msg.SeqNum == cli.seqRecv+1 { // expected sequence
+		cli.seqRecv++
+		s.readBuffer = append(s.readBuffer,
+			&readRep{msg.ConnID, msg.Payload, nil})
+
+		l := len(cli.recvDataList)
+		for i := 0; i < l; i++ {
+			if m, ok := cli.recvDataList[cli.seqRecv+1]; ok {
+				cli.seqRecv++
+				s.readBuffer = append(s.readBuffer,
+					&readRep{m.ConnID, m.Payload, nil})
+				delete(cli.recvDataList, cli.seqRecv)
+			} else {
+				break
+			}
+
+		}
+	} else if msg.SeqNum > cli.seqRecv { // order mixed up
+		cli.recvDataList[msg.SeqNum] = msg
+	} // else meaning already received
+
+}
+
+// invoked by mainRoutine, handle ack msg
+func (s *server) handleAckMsg(wrapper *msgWrapper) {
+	addr := wrapper.addr
+	msg := wrapper.msg
+	cli := s.clientIdMap[msg.ConnID]
+
+	cli.lastEpoch = s.epochCnt
+	if cli.isLost || cli.isClosed || cli.addr.String() != addr.String() {
+		return
+	}
+	if _, ok := cli.notAckMsgList[msg.SeqNum]; ok {
+		delete(cli.notAckMsgList, msg.SeqNum)
+		// trigger sliding window shift
+		cli.seqAck++
+		if len(cli.writeBuffer) > 0 {
+			m := cli.writeBuffer[0]
+			sendMsg(m, cli.addr, s.conn)
+			// put this new sent msg into not ack list
+			cli.notAckMsgList[m.SeqNum] = m
+			cli.writeBuffer = cli.writeBuffer[1:]
+
+		}
+	}
+	if cli.closeSig &&
+		len(cli.notAckMsgList) == 0 && len(cli.writeBuffer) == 0 {
+		cli.isClosed = true
+		s.clientCnt--
+		s.readBuffer = append(s.readBuffer, &readRep{0, nil, clientClosedErr})
+	}
+}
+
+// invoked by mainRoutine, handle Write() request
+func (s *server) handleWriteReq(wq writeReq) {
+	connID := wq.connID
+	payload := wq.payload
+	cli, err := s.getClientByConnID(connID)
+	s.wRepChan <- err
+	if err != nil {
+		return
+	}
+	cli.seqSend++
+	msg := NewData(connID, cli.seqSend, len(payload), payload)
+	if msg.SeqNum <= cli.seqAck+s.windowSize {
+		cli.notAckMsgList[msg.SeqNum] = msg
+		sendMsg(msg, cli.addr, s.conn)
+	} else {
+		cli.writeBuffer = append(cli.writeBuffer, msg)
+	}
+}
+
+// invoked by mainRoutine, handle Read() request
+// either when request just received or when data received
+func (s *server) handleReadReq() {
+	if s.closeSig {
+		s.rReqFlag = false
+		s.rRepChan <- &readRep{0, nil, serverCloseErr}
+		return
+	}
+	if len(s.readBuffer) > 0 {
+		s.rReqFlag = false
+		s.rRepChan <- s.readBuffer[0]
+		// remove the msg
+		s.readBuffer = s.readBuffer[1:]
+
+	}
+}
+
+// handle epoch, invoked by mainRoutine:
+// 1) If no data msg have been received from the client, then resend an ACK
+// msg for the client’s connection request.
+// 2) For each data msg that has been sent, but not yet ACK, resend the data msg.
+func (s *server) handleEpoch() {
+	s.epochCnt++
+	for connID, cli := range s.clientIdMap {
+		if !cli.isLost && !cli.isClosed {
+			// if not receiving any msg more than epochLimit, then conn lost
+			if s.epochCnt-cli.lastEpoch > s.epochLimit {
+				if s.closeSig {
+					s.shutdownAllErr <- clientLostErr
+				}
+				cli.isLost = true
+				// only remove the addr so it can init a new connect
+				delete(s.clientAddrMap, cli.addr)
+				s.clientCnt--
+
+				s.readBuffer = append(s.readBuffer,
+					&readRep{0, nil, clientLostErr})
+			}
+			if !cli.isLost {
+				// if no data msg have been received
+				if cli.seqRecv == 0 {
+					msg := NewAck(connID, 0)
+					sendMsg(msg, cli.addr, s.conn)
+				}
+				// for every msg that sent but not acked, resend
+				for _, v := range cli.notAckMsgList {
+					sendMsg(v, cli.addr, s.conn)
+				}
+			}
+		}
+	}
+}
+
+func (s *server) handleConnClose(connID int) {
+	cli, err := s.getClientByConnID(connID)
+	s.closeConnRepChan <- err
+	if err != nil {
+		return
+	}
+	if len(cli.writeBuffer) == 0 && len(cli.notAckMsgList) == 0 {
+		cli.isClosed = true
+		s.clientCnt--
+		s.readBuffer = append(s.readBuffer, &readRep{0, nil, clientLostErr})
+	} else {
+		cli.closeSig = true
+	}
+}
+
+func (s *server) mainRoutine() {
+	for {
+		select {
+		case <-s.exitChan:
+			return
+		case <-s.shutdownAll:
+			s.closeSig = true
+			for _, cli := range s.clientIdMap {
+				if !cli.isClosed && !cli.isLost {
+					cli.closeSig = true
+				}
+			}
+		case addr := <-s.connMsgChan: // chan closed at Close()
+			s.handleConnMsg(addr)
+		case msg := <-s.dataMsgChan: // chan closed at Close()
+			s.handleDataMsg(msg)
+		case msg := <-s.ackMsgChan:
+			s.handleAckMsg(msg)
+		case <-s.rReqChan:
+			s.rReqFlag = true
+		case wq := <-s.wReqChan:
+			s.handleWriteReq(wq)
+		case <-s.epochChan:
+			s.handleEpoch()
+		case connID := <-s.closeConnReqChan:
+			s.handleConnClose(connID)
+		}
+		if s.rReqFlag {
+			s.handleReadReq()
+		}
+		if s.clientCnt == 0 && s.closeSig {
+			s.shutdownAllFin <- struct{}{}
+		}
+	}
+}
+
+// readFromUDP will block until some msg comes
+// start a go routine to handle incoming msg
+func (s *server) receiveMsgRoutine() {
+	for {
+		select {
+		case <-s.exitChan:
+			return
+		default:
+			msg, addr := recvMsg(s.conn)
+			if msg == nil { // ignore wrong msg
+				continue
+			}
+			switch msg.Type {
+			case MsgConnect:
+				s.connMsgChan <- &msgWrapper{msg, addr}
+			case MsgData:
+				s.dataMsgChan <- &msgWrapper{msg, addr}
+			case MsgAck:
+				s.ackMsgChan <- &msgWrapper{msg, addr}
+			default: //ignore undefined msg type
+				logger.Println(undefMsgTypeErr)
+			}
+		}
+	}
+}
+
+func (s *server) epochRoutine() {
+	for {
+		select {
+		case <-s.exitChan:
+			return
+		case <-time.After(time.Duration(s.epochMillis) * time.Millisecond):
+			s.epochChan <- struct{}{}
+		}
+	}
+}
+
+func (s *server) getClientByConnID(connID int) (*clientInfo, error) {
+	if cli, ok := s.clientIdMap[connID]; ok {
+		if cli.isLost {
+			return nil, clientLostErr
+		} else if cli.isClosed || cli.closeSig || s.closeSig {
+			return nil, clientClosedErr
+		} else {
+			return cli, nil
+		}
+	} else {
+		return nil, clientNotExistErr
+	}
+}
+
+// Read reads a data message from a client and returns its payload,
+// and the connection ID associated with the client that sent the message.
+// This method should block until data has been received from some client.
+// It should return a non-nil error if either (1) the connection to some
+// client has been explicitly closed, (2) the connection to some client
+// has been lost due to an epoch timeout and no other messages from that
+// client are waiting to be returned, or (3) the server has been closed.
+// In the first two cases, the client's connection ID and a non-nil
+// error should be returned. In the third case, an ID with value 0 and
+// a non-nil error should be returned.
+
+// Close() desc said no Read after Close(), conflicting 3)
+// Based on the test case in lsp3_test.go, all client lost need to be returned
+// in Read, so if a client lost, then its lost err should be put into the read
+// buffer for future Read() invoke.
 func (s *server) Read() (int, []byte, error) {
-	// TODO: remove this line when you are ready to begin implementing this method.
-	select {} // Blocks indefinitely.
-	return -1, nil, errors.New("not yet implemented")
+	s.rReqChan <- struct{}{}
+	reply := <-s.rRepChan
+	return reply.connID, reply.payload, reply.err
 }
 
+// Write sends a data message to the client with the specified connection ID.
+// This method should NOT block, and should return a non-nil error if the
+// connection with the client has been lost.
 func (s *server) Write(connID int, payload []byte) error {
-	return errors.New("not yet implemented")
+	if len(payload) > MAX_MSG_SIZE {
+		return msgTooLargeErr
+	}
+	s.wReqChan <- writeReq{connID, payload}
+	err := <-s.wRepChan
+	return err
 }
 
+// CloseConn terminates the client with the specified connection ID, returning
+// a non-nil error if the specified connection ID does not exist. All pending
+// messages to the client should be sent and acknowledged. However, unlike Close,
+// this method should NOT block.
 func (s *server) CloseConn(connID int) error {
-	return errors.New("not yet implemented")
+	s.closeConnReqChan <- connID
+	err := <-s.closeConnRepChan
+	return err
 }
 
+// Close terminates all currently connected clients and shuts down the LSP server.
+// This method should block until all pending messages for each client are sent
+// and acknowledged. If one or more clients are lost during this time, a non-nil
+// error should be returned. Once it returns, all goroutines running in the
+// background should exit.
+//
+// You may assume that Read, Write, CloseConn, or Close will not be called after
+// calling this method.
 func (s *server) Close() error {
-	return errors.New("not yet implemented")
+	// only receive ack msg from client, and send msg to client
+	// not receiveing conn and data msg
+	s.shutdownAll <- struct{}{}
+	var err error = nil
+	for {
+		select {
+		case err = <-s.shutdownAllErr:
+		case <-s.shutdownAllFin:
+			close(s.exitChan) // close several go routines
+			s.conn.Close()
+			return err
+		}
+	}
+
+}
+
+func sendMsg(msg *Message, addr *lspnet.UDPAddr, conn *lspnet.UDPConn) {
+	b := MarshalMsg(msg)
+	if b == nil {
+		// suggest sth wrong with the implementation
+		logger.Fatalln("Fatal error in Marshal at sendMsg")
+	}
+	// WriteToUDP is only good for server, on client should use Write
+	// No error will be thrown and no blocking even if connection has lost
+	conn.WriteToUDP(b, addr)
+}
+
+func recvMsg(conn *lspnet.UDPConn) (*Message, *lspnet.UDPAddr) {
+	buffer := make([]byte, 1024)
+	// ReadFromUDP blocks until something comes in
+	size, addr, err := conn.ReadFromUDP(buffer)
+	if err != nil { // error might caused by connection closed
+		return nil, addr
+	}
+	msg := UnmarshalMsg(buffer[:size]) // if any error in unmarshal, msg = nil
+	if msg == nil {
+		logger.Println("Error in Unmarshal at recvMsg")
+	}
+	return msg, addr
 }
